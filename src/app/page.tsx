27ee3853e6c037dab464b0d9c515271520cba7d5
kamdem-rudy <kamdemrudycabrel.@gmail.com>
2026-06-16 23:59:57 +0200

    fix pages principales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // src/app/page.tsx
+export const dynamic = 'force-dynamic'
 import { prisma } from '@/lib/prisma'
 import { StatutCommande, StatutLivraison } from '@prisma/client'
 import GraphWrapper from '@/components/graph/GraphWrapper'
